bb1b76afd4d98434a36321cb02854556bdc98be7
vilelalabs <hvilelaeng@gmail.com>
2022-08-17 00:28:13 -0300

    use of args,clean output folder, correct filenames

--- divider.py
+++ divider.py
@@ -3,21 +3,18 @@ import numpy as np
 import soundfile as sf
 
 
-
-
-def divideIntoParts(fileName):
+def divideIntoParts(fileName, mms= 1000000, thr= 0.001, std = 49000):
 
     data, samplerate = sf.read(fileName)
 
     selectedData = np.zeros((len(data), 2))
-
     
     countLowData = 0
     cut_point = len(data)-1
     
-    minimum_music_samples = 1000000
-    threshold = 0.001 # default 0.001  
-    silent_time_detection = 49000 # default 49000        
+    minimum_music_samples = mms # default 1000000 -  aprox 20.8s of music
+    threshold = thr             # default 0.001  
+    silent_time_detection = std # default 49000 - approx 1s of silence        
     i = 0
 
     # while there is data to be processed
@@ -25,11 +22,9 @@ def divideIntoParts(fileName):
     while len(data) > minimum_music_samples: 
         for x in range(len(data)):
 
-            # garante cortes apenas após o início da música (após aprox: 20,8 segundos)
             if x >= minimum_music_samples: 
                 if(data[x][0] < threshold and data[x][1] < threshold and data[x][0] > -threshold and data[x][1] > -threshold):
                     countLowData += 1
-                    # approx 1 second of low volume
                     if(countLowData >= silent_time_detection):
                         cut_point = x
 
--- main.py
+++ main.py
@@ -4,14 +4,46 @@ import glob
 import os
 import divider as dv
 
-filename = 'audio'
+import sys
+
+filename = sys.argv[1]
+
+# standard parameters
+mms = 1000000
+thr = 0.001
+std = 49000
+
+for i in range(2, len(sys.argv)):
+    if sys.argv[i] == "--mms":
+        mms = int(sys.argv[i+1])
+    if sys.argv[i] == "--thr":
+        thr = float(sys.argv[i+1])
+    if sys.argv[i] == "--std":
+        std = int(sys.argv[i+1])
+    if sys.argv[i] == "--help":
+        print("Possible arguments:")
+        print("--mms: minimum music samples")
+        print("--thr: threshold")
+        print("--std: silent time detection")
+        print("--help: show this help")
+        print(" Go to project on github (https://github.com/vilelalabs/audio-music-divider) for more details.")
+        exit()
+
 
 print("Loading file...")
-song = AudioSegment.from_mp3(f"{filename}.mp3")
-print("Converting file to wav...")
-song.export(f"{filename}.wav", format="wav")
+song = AudioSegment.from_mp3(filename)
 
-dv.divideIntoParts(f"{filename}.wav")
+print("Cleaning up output folder...")
+files = glob.glob(f"output/*")
+for file in files:
+    os.remove(file)
+
+print("Converting file to wav...")
+wavFilename = filename.replace(".mp3", ".wav")
+
+song.export(wavFilename, format="wav")
+
+dv.divideIntoParts(wavFilename, mms, thr, std)
 print("\n")
 
 print("Converting to mp3 and erasing wav files...",end='')
@@ -19,7 +51,7 @@ files = glob.glob(f"output/*.wav")
 for file in files:
     print(".",end='', flush=True)
     song = AudioSegment.from_wav(file)
-    song.export(f"{file}.mp3", format="mp3")
+    song.export(f"{file[0:-4]}.mp3", format="mp3")
     os.remove(file)
-os.remove(f"{filename}.wav")
+os.remove(wavFilename)
 print("\nDone!")
